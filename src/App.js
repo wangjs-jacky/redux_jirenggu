19cf61646ee380da08e78baafc3e20363fea95fd
wangjs-jacky <2409277719@qq.com>
2022-02-13 01:49:13 +0800

    createNewState规范化data创建的immutable特性

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ const App = () => {
     </appContext.Provider>
   );
 }
-const Child1 = ()=> {
+const Child1 = () => {
   return (
     <Section>
       兄弟组件1
-      <User/>
+      <User />
     </Section>
   )
 }
-const Child2 = ()=> {
+const Child2 = () => {
   return (
     <Section>
       兄弟组件2
@@ -45,7 +45,7 @@ const Child2 = ()=> {
     </Section>
   )
 }
-const Child3 = ()=> {
+const Child3 = () => {
   return (
     <Section>
       兄弟组件3
@@ -53,7 +53,7 @@ const Child3 = ()=> {
   )
 }
 
-const User = ()=>{
+const User = () => {
   const contextValue = useContext(appContext)
   return (
     <div>
@@ -62,18 +62,42 @@ const User = ()=>{
   )
 }
 
-const UserModifier = ()=>{
-  const contextValue = useContext(appContext)
-  const onChange = (e)=>{
-    contextValue.appState.user.name = e.target.value
-    contextValue.setAppState({...contextValue.appState})
+// 创建一个新的状态需要几个要素：
+// 1. 修改的是哪儿个 数据源(状态仓库)
+// 2. 对这个仓库中的什么变量做什么操作？
+// 3. 传递的数据，即payload（payload这个词很形象，载荷）
+const createNewState = (state, actionType, payload) => {
+  if (actionType === "updateUser") {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload
+      }
+    }
+  } else {
+    return state
+  }
+}
+
+const UserModifier = () => {
+  const { appState, setAppState } = useContext(appContext)
+  const onChange = (e) => {
+    // 编写的时候已经发现存在问题了，即没有拷贝一份后再修改。
+    // contextValue.appState.user.name = e.target.value
+    // contextValue.setAppState({...contextValue.appState})
+
+    // 通过newState创建出来的变量，符合 React 对状态Immutable的要求
+    const newState = createNewState(appState, "updateUser", { name: e.target.value })
+    console.log('newState', newState);
+    setAppState(newState)
   }
   return (
     <div>
       修改用户名：
-      <input 
-      value={contextValue.appState.user.name}
-      onChange={onChange}></input>
+      <input
+        value={appState.user.name}
+        onChange={onChange}></input>
     </div>
   )
 
