fc49d9af2292e90b48c24807602972c400562aa3
wangjs-jacky <2409277719@qq.com>
2022-02-13 20:56:31 +0800

    封装store的subscribe方法,一旦某个状态发生改变,则触发所有订阅这个事件(connect实现)的组件视图更新

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import styled from "styled-components"
 
 // 创建一个全局的环境
@@ -15,11 +15,6 @@ const Section = styled.section`
     border: 1px dashed #333;
   }
 `
-
-// 使用初衷：在Hooks的渲染原则是：一旦调用了setAppState，App会进行重复渲染，于是底下所有组件全部被重复渲染了。
-// 解决：直接废弃掉 Hooks 提供的 setAppState 方法。
-// 做法：自己创建 store 对象，自己维护 state 和 修改这个 state 的方法
-// 依次来替换掉 原始 的 react-Hooks 自己提供的钩子函数。
 const store = {
   appState: {
     user: { name: "王家盛", age: 18 }
@@ -27,6 +22,22 @@ const store = {
   setAppState(newState) {
     console.log('newState', newState);
     store.appState = newState
+    store.listeners.map(fn => fn(store.appState))
+  },
+  /*
+    通过 [,update] = useState()刷新视图的方法存在一个问题：
+    createWrapper(connect) 会单独生成一个dispatch函数, 于是每一个connect的组件，只会刷新自己的状态，
+    而无法把这个 state 的变化 映射到 所有依赖这个state的组件中。
+    解决方法： 使用 eventhub，订阅 state 的变化。
+    一旦某个state，就将 全局订阅state变化的组件 给渲染一下。
+  */
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      const index = store.listeners.indexOf(fn)
+      store.listeners.splice(index, 1)
+    }
   }
 }
 
@@ -68,15 +79,33 @@ const Child3 = () => {
   )
 }
 
-const User = () => {
+// 使用 createWrapper 批量化生成 HOC 组件 ,即connect
+const createWrapper = (Component) => {
+  return (props) => {
+    const { appState, setAppState } = useContext(appContext)
+    // 显式地调用 setXXXX 方法，达到精准的控制 视图刷新 的功能
+    const [, update] = useState({})
+    useEffect(() => {
+      store.subscribe(() => {
+        update({})
+      })
+    }, [])
+
+    const dispatch = (actionType, payload) => {
+      setAppState(createNewState(appState, actionType, payload))
+    }
+    return <Component {...props} dispatch={dispatch} state={appState} />
+  }
+}
+
+const User = createWrapper(({ state }) => {
   console.log('渲染User');
-  const { appState } = useContext(appContext)
   return (
     <div>
-      用户名：{appState.user.name}
+      用户名：{state.user.name}
     </div>
   )
-}
+})
 
 const createNewState = (state, actionType, payload) => {
   if (actionType === "updateUser") {
@@ -108,21 +137,6 @@ const UserModifier = ({ dispatch, state, children }) => {
     </div>
   )
 
-}
-
-// 使用 createWrapper 批量化生成 HOC 组件 ,即connect
-const createWrapper = (Component) => {
-  return (props) => {
-    const { appState, setAppState } = useContext(appContext)
-    // 显式地调用 setXXXX 方法，达到精准的控制 视图刷新 的功能
-    const [, update] = useState({})
-    const dispatch = (actionType, payload) => {
-      setAppState(createNewState(appState, actionType, payload))
-      // 在 dispatch 后刷新
-      update({})
-    }
-    return <Component {...props} dispatch={dispatch} state={appState} />
-  }
 }
 
 const Wrapper2 = createWrapper(UserModifier)
